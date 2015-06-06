b70485f837862fa1805ed33c7a4701d5d4cefc4e
Naoya Inoue@Whiskey <naoya-i@ecei.tohoku.ac.jp>
2015-06-06 13:12:45 +0900

    Removed trash again.

--- src/linepick.cpp
+++ src/linepick.cpp
@@ -11,15 +11,6 @@
 
 #include "optparse.h"
 
-std::string _replaceAll(std::string str, const std::string& from, const std::string& to) {
-  size_t start_pos = 0;
-  while((start_pos = str.find(from, start_pos)) != std::string::npos) {
-    str.replace(start_pos, from.length(), to);
-    start_pos += to.length(); // Handles case where 'to' is a substring of 'from'
-  }
-  return str;
-}
-
 int main(int argc, char** argv) {
   optparse_t opts("Usage:\n  linepick"
                                     " -i <INPUT FILE>"
